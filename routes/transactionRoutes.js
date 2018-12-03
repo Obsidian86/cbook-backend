5818ad70ddaaf112a0412c28e9bfe7bb37491fec
Christopher <christopher13e11@gmail.com>
2018-12-03 17:01:58 -0600

    update

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -7,20 +7,39 @@ router.post("/", async(req, res, next)=>{
     try {
         let foundAccount = await Account.findById(req.body.account_id); 
         let createdTransaction = await Transaction.create( req.body.transaction );
+        foundAccount.balance = parseInt(foundAccount.balance) + parseInt(createdTransaction.amount);
         await foundAccount.transactions.push(createdTransaction);
-        foundAccount.save();
-        res.status(200).json({synced: 200}); 
+        await foundAccount.save();
+        res.status(200).json({newTrans: createdTransaction, synced: Date.now()}); 
     } catch (err) {  
         next({message: err.message});
-    }
-    
-   
+    } 
 });
-router.delete("/deletetransaction", (req, res)=>{
-    res.status(200).json(accountData);
+
+router.delete("/", async (req, res, next)=>{
+        console.log(req.body);
+    try {
+        let foundAccount = await Account.findById(req.body.accountId); 
+        let findTransaction = await Transaction.findById(req.body.transId);  
+        foundAccount.balance = parseFloat(foundAccount.balance) - parseFloat(findTransaction.amount);
+        foundAccount.transactions = foundAccount.transactions.filter(acc => acc.toString() !== req.body.transId);
+        await foundAccount.save();
+        res.status(200).json({synced: Date.now()}); 
+    } catch (err) {  
+        next({message: err.message});
+    } 
 });
-router.put("/updatetransaction", (req, res)=>{
-    res.status(200).json(accountData);
+router.put("/", async (req, res, next)=>{
+    let foundAccount = await Account.findById(req.body.account_id); 
+    let findTransaction = await Transaction.findById(req.body.transaction_id); 
+    foundAccount.balance = (parseFloat(foundAccount.balance) - parseFloat(findTransaction.amount)) + parseFloat(req.body.transaction.amount);
+    findTransaction.payee = req.body.transaction.payee;
+    findTransaction.amount = req.body.transaction.amount;
+    findTransaction.cleared = req.body.transaction.cleared;
+    findTransaction.date = req.body.transaction.date;
+    await foundAccount.save();
+    await findTransaction.save();
+    res.status(200).json({synced: Date.now()});
 });
 
 module.exports = router;
